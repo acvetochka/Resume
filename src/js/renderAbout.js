80f71964785bd4eecf160e25546166f454045322
Alona Kuznietsova <acvetochka@gmail.com>
2023-12-02 22:39:16 +0100

    change mobile-version

--- src/js/renderAbout.js
+++ src/js/renderAbout.js
@@ -1,14 +1,19 @@
 import about from '../data/about.json';
+import photo from '../img/alona-kuznietsova.jpg';
 
 const aboutSection = document.querySelector('.about');
 
 function renderAbout() {
   const markup = about
     .map(
-      ({ name, position, description }) => `<h1 class="about-name">${name}</h1>
+      ({ name, position, description }) => `
+      <img src=${photo} alt="photo" class="photo">
+      <div>
+      <h1 class="about-name">${name}</h1>
         <h2 class="about-position">${position}</h2>
         <p class="about-description">${description}
-        </p>`
+        </p>
+        </div>`
     )
     .join('');
   aboutSection.innerHTML = markup;
