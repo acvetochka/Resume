5875c562f13ae128476b8c9b1d506f66b5518544
Alona Kuznietsova <acvetochka@gmail.com>
2023-12-17 22:17:36 +0100

    change photo-styles

--- src/js/renderAbout.js
+++ src/js/renderAbout.js
@@ -2,12 +2,14 @@ import about from '../data/about.json';
 import photo from '../img/alona-kuznietsova.jpg';
 
 const aboutSection = document.querySelector('.about');
-
+  
 function renderAbout() {
   const markup = about
     .map(
       ({ name, position, description }) => `
-      <img src=${photo} alt="photo" class="photo">
+      <div class="photo-wrapper"> 
+        <img src=${photo} alt="photo" class="photo" width="250" height="250">
+      </div>
       <div>
       <h1 class="about-name">${name}</h1>
         <h2 class="about-position">${position}</h2>
