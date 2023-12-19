8556e58d06f1509e77e1827fa51fc26c6d3cffbb
Alona Kuznietsova <acvetochka@gmail.com>
2023-12-19 22:28:21 +0100

    add german

--- src/js/renderAbout.js
+++ src/js/renderAbout.js
@@ -1,12 +1,26 @@
 import about from '../data/about.json';
+import aboutDe from '../data/de/about.json';
 import photo from '../img/alona-kuznietsova.jpg';
 
 const aboutSection = document.querySelector('.about');
-  
+const currentURL = window.location.href;
+let aboutData = about;
+
 function renderAbout() {
-  const markup = about
+  if (currentURL.includes('de.html')) {
+    aboutData = aboutDe;
+  } else {
+    aboutData = about;
+  }
+
+  console.log(currentURL);
+
+  const markup = aboutData
     .map(
-      ({ name, position, description }) => `
+      ({ name, position, description, link, lang }) => `
+      <div class="lang">
+        <a href=${link} class="lang-link">${lang}</a>
+      </div>
       <div class="photo-wrapper"> 
         <img src=${photo} alt="photo" class="photo" width="250" height="250">
       </div>
