6dd21e9d2710422d5fe10333a374490ba659a4c4
Alona Kuznietsova <acvetochka@gmail.com>
2024-07-25 23:10:01 +0200

    add icons

---
+++ src/data/contacts.js
@@ -0,0 +1,56 @@
+import email from '../img/email.svg';
+import linkedin from '../img/linkedin.svg';
+import phone from '../img/phone.svg';
+import whatsapp from '../img/whatsapp.svg';
+import github from '../img/github.svg';
+
+
+
+export const contacts = [
+  {
+    "name": "phone",
+    "href": "tel:+4915141896120",
+    "text": "+49(151)418-96-120",
+    "target": "_self",
+    "src": phone
+  },
+  {
+    "name": "email",
+    "href": "mailto:acvetochka@gmail.com",
+    "text": "acvetochka@gmail.com",
+    "target": "_self",
+    "src": email
+  },
+  {
+    "name": "phone",
+    "href": "https://t.me/acvetochka",
+    "text": "Telegram",
+    "target": "_blank",
+    "rel": "noopener noreferrer nofollow",
+    "src": phone
+  },
+  {
+    "name": "whatsapp",
+    "href": "https://wa.me/30961270693",
+    "text": "WhatsApp",
+    "target": "_blank",
+    "rel": "noopener noreferrer nofollow",
+    "src": whatsapp
+  },
+  {
+    "name": "linkedin",
+    "href": "https://www.linkedin.com/in/alona-kuznietsova/",
+    "text": "LinkedIn",
+    "target": "_blank",
+    "rel": "noopener noreferrer nofollow",
+    "src": linkedin
+  },
+  {
+    "name": "github",
+    "href": "https://github.com/acvetochka",
+    "text": "GitHub",
+    "target": "_blank",
+    "rel": "noopener noreferrer nofollow",
+    "src": github
+  }
+]
--- src/js/renderContacts.js
+++ src/js/renderContacts.js
@@ -1,38 +1,8 @@
-import contacts from '../data/contacts.json';
-import email from '../img/email.svg';
-import linkedin from '../img/linkedin.svg';
-import phone from '../img/phone.svg';
-import whatsapp from '../img/whatsapp.svg';
-import github from '../img/whatsapp.svg';
-
-
+// import contacts from '../data/contacts.json';
+import {contacts} from '../data/contacts.js';
 
 const contactsList = document.querySelector('.contacts-list');
 
-function getIcon(name, src){
-  switch(name){
-    case "phone":
-      src = phone;
-      break;
-    case "email":
-      src = email;
-      break;
-    case "linkedin":
-      src = linkedin;
-      break;
-    case "whatsapp":
-      src = whatsapp;
-      break;
-    case "github":
-      src = github;
-      break;
-    case "telegram":
-      src= phone;
-      break;
-    default:
-      break;
-    }
-}
 
 let src = "";
 
@@ -41,11 +11,11 @@ function renderContacts() {
   const markup = contacts
     .map(
       ({ name, href, text, target, rel, src }) => {
-        // getIcon(name, src);
         console.log(src);
 
         return `<li class="contact-item">
    <a href=${href} rel="noopener noreferrer nofollow" target=${target} class="contact-link">
+   <img width="20" height="20" src=${src}/>
    ${text}</a></li>`}
   )
   .join('');
