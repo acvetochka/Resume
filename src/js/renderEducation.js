3622683b350879bb6ee31e1bc9286ea981d16eaf
Alona Kuznietsova <acvetochka@gmail.com>
2025-10-15 23:23:52 +0200

    change resume-structure and add new expirience

--- src/js/renderEducation.js
+++ src/js/renderEducation.js
@@ -1,37 +1,37 @@
-import eduEn from '../data/education.json';
-import eduDe from '../data/de/education.json';
+// import eduEn from '../data/education.json';
+// import eduDe from '../data/de/education.json';
 
-const eduList = document.querySelector('.education-list');
-const currentURL = window.location.href;
-let edu = eduEn;
+// const eduList = document.querySelector('.education-list');
+// const currentURL = window.location.href;
+// let edu = eduEn;
 
-function renderEducation() {
-  if (currentURL.includes('de.html')) {
-    edu = eduDe;
-  } else {
-    edu = eduEn;
-  }
+// function renderEducation() {
+//   if (currentURL.includes('de.html')) {
+//     edu = eduDe;
+//   } else {
+//     edu = eduEn;
+//   }
 
-  const markup = edu
-    .map(({ uni, date, location, spec, degree }) => {
-      const deg = degree => {
-        if (degree) {
-          return ' | ' + degree;
-        } else {
-          return '';
-        }
-      };
+//   const markup = edu
+//     .map(({ uni, date, location, spec, degree }) => {
+//       const deg = degree => {
+//         if (degree) {
+//           return ' | ' + degree;
+//         } else {
+//           return '';
+//         }
+//       };
 
-      return `<li class="education-item">
-      <h4 class="education-spec">${degree}</h4>
-      <p class="education-uni">${uni}</p>
-      <p class="period">${date}<span class="line"> | </span>${location}</p>
-      </li>`;
-    })
-    .join('');
-    
-    // ${deg(degree)}</p>
-  eduList.innerHTML = markup;
-}
+//       return `<li class="education-item">
+//       <h4 class="education-spec">${degree}</h4>
+//       <p class="education-uni">${uni}</p>
+//       <p class="period">${date}<span class="line"> | </span>${location}</p>
+//       </li>`;
+//     })
+//     .join('');
 
-renderEducation();
+//     // ${deg(degree)}</p>
+//   eduList.innerHTML = markup;
+// }
+
+// renderEducation();
--- src/js/renderFurtherEducation.js
+++ src/js/renderFurtherEducation.js
@@ -1,39 +1,39 @@
-import furtherEn from '../data/addition.json';
-import furtherDe from '../data/de/addition.json';
+// import furtherEn from '../data/addition.json';
+// import furtherDe from '../data/de/addition.json';
 
-const furtherList = document.querySelector('.further-list');
-const currentURL = window.location.href;
-let further = furtherEn;
+// const furtherList = document.querySelector('.further-list');
+// const currentURL = window.location.href;
+// let further = furtherEn;
 
-function renderFurtherEducation() {
-  if (currentURL.includes('de.html')) {
-    further = furtherDe;
-  } else {
-    further = furtherEn;
-  }
+// function renderFurtherEducation() {
+//   if (currentURL.includes('de.html')) {
+//     further = furtherDe;
+//   } else {
+//     further = furtherEn;
+//   }
 
-  const markup = further
-    .map(
-      ({ name, period, location }) => `<li class="further-item">
-            <div class="name_container"><h3 class="further-position">${name}</span>
-            </h3>
-            <p class="company">${location}</p>
-            </div>
-            <p class="period">${period}</p>
-            
-            </li>`
-    )
-    .join('');
-  furtherList.innerHTML = markup;
-}
+//   const markup = further
+//     .map(
+//       ({ name, period, location }) => `<li class="further-item">
+//             <div class="name_container"><h3 class="further-position">${name}</span>
+//             </h3>
+//             <p class="company">${location}</p>
+//             </div>
+//             <p class="period">${period}</p>
 
-// <span class="line">|</span>
-{
-  /* <ul class="further-list">
-${duties
-  .map(duty => `<li class="further-list-item">${duty}</li>`)
-  .join('')}
-</ul> */
-}
+//             </li>`
+//     )
+//     .join('');
+//   furtherList.innerHTML = markup;
+// }
 
-renderFurtherEducation();
+// // <span class="line">|</span>
+// {
+//   /* <ul class="further-list">
+// ${duties
+//   .map(duty => `<li class="further-list-item">${duty}</li>`)
+//   .join('')}
+// </ul> */
+// }
+
+// renderFurtherEducation();
