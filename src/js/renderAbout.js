240d46dbf7b51cc8e59ed79468844052de865c05
Alona Kuznietsova <acvetochka@gmail.com>
2025-10-25 14:23:13 +0200

    add pdf-file

--- src/js/renderAbout.js
+++ src/js/renderAbout.js
@@ -1,7 +1,8 @@
 import about from '../data/about.json';
 import aboutDe from '../data/de/about.json';
-// import photo from '../img/alona-kuznietsova.jpg';
 import photo from '../img/alonaKuz.jpg';
+// import pdf from '../files/Lebenslauf_Frontend_Developer_Kuznietsova.pdf';
+// import pdfDe from '../files/Lebenslauf_Frontend_Developer_Kuznietsova.pdf';
 
 const aboutSection = document.querySelector('.about');
 const aboutContainer = document.querySelector('.about-container');
@@ -11,15 +12,18 @@ let aboutData = about;
 function renderAbout() {
   if (currentURL.includes('de.html')) {
     aboutData = aboutDe;
+    // pdfFile = pdfDe;
   } else {
     aboutData = about;
+    // pdfFile = pdf;
   }
 
   const markup = aboutData
     .map(
-      ({ name, position, technologies, description, link, lang }) => `
+      ({ name, position, technologies, description, link, lang, pdf }) => `
       <div class="about-container">
        <div class="lang">
+       <a href=${pdf} download>pdf</a>
         <a href=${link} class="lang-link">${lang}</a>
        </div>
        <div class="photo-wrapper"> 
