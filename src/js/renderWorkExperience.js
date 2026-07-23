5599bba6739b2c26f24d3aafffb2578ee5b88ca0
Alona Kuznietsova <acvetochka@gmail.com>
2026-07-23 23:05:28 +0200

    update experience

--- src/js/renderWorkExperience.js
+++ src/js/renderWorkExperience.js
@@ -14,17 +14,38 @@ function renderWorkExperience() {
 
   const markup = work
     .map(
-      ({ position, date, company, place, duties }) => `<li class="company-item">
+      ({
+        position,
+        date,
+        company,
+        place,
+        duties,
+        technologies = [],
+        reference,
+      }) => `<li class="company-item">
             <div class="position-container"><h3 class="work-position">${position}
             </h3>
             <p class="period">${date}</p>
             </div>
             <p class="company">${company}<span class="line"> | </span>${place}</p>
+            <ul class="technologies-list">
+            ${technologies
+              .map(skill => `<li class="technology-item">${skill}</li>`)
+              .join('')}
+            </ul>
             <ul class="work-list">
             ${duties
               .map(duty => `<li class="work-list-item">${duty}</li>`)
               .join('')}
             </ul>
+            ${
+              (reference &&
+                `<div class="reference-container"><p class="text">Reference: </p>
+               <a href=${reference.website} target="_blank" class="project-link reference-link">Website</a> |
+              <a href=${reference.github} target="_blank" class="project-link reference-link">GitHub</a>
+              </div>`) ||
+              ''
+            }
           </li>`
     )
     .join('');
